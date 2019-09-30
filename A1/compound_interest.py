f428cb2ca73920652d581f60ae74a194a6541a1c
Amy Tan <atan59@my.bcit.ca>
2019-09-29 23:00:53 -0700

    Simplified compound_interest function. Removed unnecessary functions.

--- A1/compound_interest.py
+++ A1/compound_interest.py
@@ -2,116 +2,6 @@
 import doctest
 
 
-def add(number_1, number_2):
-    """
-    Add two numbers together.
-
-    :param number_1: an int or float
-    :param number_2: an int or float
-    :precondition: number_1 must ba an int or a float
-    :precondition: number_2 must be an int or a float
-    :postcondition: add number_1 and number_2 together
-    :return: an int or float
-    >>> add(0, 0)
-    0
-    >>> add(1, 1)
-    2
-    >>> add(-1, 3)
-    2
-    >>> add(30.5, 2)
-    32.5
-    >>> add(-2.5, 5)
-    2.5
-    >>> add(-1, -2)
-    -3
-    >>> add(-2.4, -6.4)
-    -8.8
-    """
-    return number_1 + number_2
-
-
-def multiply(number_1, number_2):
-    """
-    Multiply two numbers together.
-
-    :param number_1: an int or a float
-    :param number_2: an int or a float
-    :precondition: number_1 must be an int or a float
-    :precondition: number_2 must be an int or a float
-    :postcondition: Multiply number_1 and number_2 together
-    :return: an int or a float
-    >>> multiply(0, 0)
-    0
-    >>> multiply(1, 1)
-    1
-    >>> multiply(0.1, 0.3)
-    0.03
-    >>> multiply(2, 0.5)
-    1.0
-    >>> multiply(-1, 1)
-    -1
-    >>> multiply(-1, -1)
-    1
-    >>> multiply(-1.5, 2)
-    -3.0
-    >>> multiply(-2.4, -1.2)
-    2.88
-    """
-    return number_1 * number_2
-
-
-def divide(dividend, divisor):
-    """
-    Divide dividend by divisor.
-
-    :param dividend: an int or a float
-    :param divisor: an int or a float
-    :precondition: dividend must be an int or a float
-    :precondition: divisor must be an int or a float not equal to 0
-    :postcondition: divide dividend by divisor
-    :return: a float
-    >>> divide(0, 1)
-    0.0
-    >>> divide(1, 1)
-    1.0
-    >>> divide(2, 0.5)
-    4.0
-    >>> divide(1.2, 0.3)
-    4.0
-    >>> divide(-1, 1.0)
-    -1.0
-    >>> divide(-2, -0.5)
-    4.0
-    """
-    return dividend / divisor
-
-
-def exponent(base, exponent_value):
-    """
-    Calculate base raised to power of exponent_value.
-
-    :param base: an int or float
-    :param exponent_value: an int or float
-    :precondition: base must be an int or a float
-    :precondition: exponent_value must ba an int or a float
-    :postcondition: Calculate base raised to the power of exponent_value
-    :return: an int or float
-    >>> exponent(1, 1)
-    1
-    >>> exponent(2, 1.0)
-    2.0
-    >>> exponent(0, 0)
-    1
-    >>> exponent(-2, 1)
-    -2
-    >>> exponent(-4, -1)
-    -0.25
-    >>> exponent(-1, -1)
-    -1.0
-    """
-    return base ** exponent_value
-
-
 def compound_interest(principal, interest_rate, times_compounded, years):
     """
     Calculate compound interest amount.
@@ -135,10 +25,14 @@ def compound_interest(principal, interest_rate, times_compounded, years):
     >>> compound_interest(10, 0.15, 4, 20)
     190.13
     """
-    amount = multiply(principal,
-                      exponent(add(1, divide(interest_rate, times_compounded)), multiply(times_compounded, years)))
+    amount = principal * ((1 + (interest_rate / times_compounded)) ** (times_compounded * years))
     return round(amount, 2)
 
 
 if __name__ == "__main__":
     doctest.testmod()
+
+
+"""
+
+"""
