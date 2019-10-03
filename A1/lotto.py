7dd256e37809baf73c1f577d102c859fd7a2de0c
atan59 <atan59@my.bcit.ca>
2019-10-03 13:14:04 -0700

    Added main code block in lotto.py.

--- A1/lotto.py
+++ A1/lotto.py
@@ -11,6 +11,10 @@ def number_generator():
     print("The lottery numbers are:", result)
 
 
+if __name__ == "__main__":
+    number_generator()
+
+
 """
 I used abstraction to ignore any details that would cause an error in the function and focused only on making it work. I
 then used automation to organize the main function into well-defined steps.
