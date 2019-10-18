34c5dad37850981757e55f5681ce56242790bc88
atan59 <atan59@my.bcit.ca>
2019-10-18 13:04:35 -0700

    Finished check_if_alive unittests.

--- A2/test_check_if_alive.py
+++ A2/test_check_if_alive.py
@@ -1,6 +1,48 @@
 from unittest import TestCase
+from unittest.mock import patch
+import io
+from A2.dungeonsanddragons import check_if_alive
 
 
-class TestCheck_if_alive(TestCase):
-    def test_check_if_alive(self):
-        self.fail()
+class TestCheckIfAlive(TestCase):
+    def test_check_if_alive_is_alive(self):
+        expected = True
+        actual = check_if_alive({'Name': "test", 'Race': "test", 'Class': "test", 'HP': [1, 1], 'Strength': 1,
+                                 'Dexterity': 1, 'Constitution': 1, 'Intelligence': 1, 'Wisdom': 1, 'Charisma': 1,
+                                 'XP': 0, 'Inventory': []})
+        self.assertEqual(expected, actual)
+
+    def test_check_if_alive_is_dead(self):
+        expected = False
+        actual = check_if_alive({'Name': "test", 'Race': "test", 'Class': "test", 'HP': [1, 0], 'Strength': 1,
+                                 'Dexterity': 1, 'Constitution': 1, 'Intelligence': 1, 'Wisdom': 1, 'Charisma': 1,
+                                 'XP': 0, 'Inventory': []})
+        self.assertEqual(expected, actual)
+
+    def test_check_if_alive_is_alive_is_bool(self):
+        actual = check_if_alive({'Name': "test", 'Race': "test", 'Class': "test", 'HP': [1, 1], 'Strength': 1,
+                                 'Dexterity': 1, 'Constitution': 1, 'Intelligence': 1, 'Wisdom': 1, 'Charisma': 1,
+                                 'XP': 0, 'Inventory': []})
+        self.assertIs(type(actual), bool)
+
+    def test_check_if_alive_is_dead_is_bool(self):
+        actual = check_if_alive({'Name': "test", 'Race': "test", 'Class': "test", 'HP': [1, 0], 'Strength': 1,
+                                 'Dexterity': 1, 'Constitution': 1, 'Intelligence': 1, 'Wisdom': 1, 'Charisma': 1,
+                                 'XP': 0, 'Inventory': []})
+        self.assertIs(type(actual), bool)
+
+    @patch("sys.stdout", new_callable=io.StringIO)
+    def test_check_if_alive_is_alive_print(self, mock_stdout):
+        expected = '\ntest is alive!\n'
+        check_if_alive({'Name': "test", 'Race': "test", 'Class': "test", 'HP': [1, 1], 'Strength': 1, 'Dexterity': 1,
+                        'Constitution': 1, 'Intelligence': 1, 'Wisdom': 1, 'Charisma': 1, 'XP': 0, 'Inventory': []})
+        actual = mock_stdout.getvalue()
+        self.assertEqual(expected, actual)
+
+    @patch("sys.stdout", new_callable=io.StringIO)
+    def test_check_if_alive_is_dead_print(self, mock_stdout):
+        expected = '\ntest is dead!\n'
+        check_if_alive({'Name': "test", 'Race': "test", 'Class': "test", 'HP': [1, 0], 'Strength': 1, 'Dexterity': 1,
+                        'Constitution': 1, 'Intelligence': 1, 'Wisdom': 1, 'Charisma': 1, 'XP': 0, 'Inventory': []})
+        actual = mock_stdout.getvalue()
+        self.assertEqual(expected, actual)
