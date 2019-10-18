d6561a9e8b9074d3210eb2d1a475b3f8c5dd5071
atan59 <atan59@my.bcit.ca>
2019-10-18 11:42:40 -0700

    Finished select_race function unittests.

--- A2/test_select_race.py
+++ A2/test_select_race.py
@@ -1,6 +1,27 @@
 from unittest import TestCase
+from unittest.mock import patch
+from A2.dungeonsanddragons import select_race
 
 
-class TestSelect_race(TestCase):
-    def test_select_race(self):
-        self.fail()
+class TestSelectRace(TestCase):
+    @patch('builtins.input', return_value=1)
+    def test_select_race_min(self, mock_input):
+        expected = 'dragonborn'
+        actual = select_race()
+        self.assertEqual(expected, actual)
+
+    @patch('builtins.input', return_value=9)
+    def test_select_race_max(self, mock_input):
+        expected = 'tiefling'
+        actual = select_race()
+        self.assertEqual(expected, actual)
+
+    @patch('builtins.input', return_value=1)
+    def test_select_race_is_string_min(self, mock_input):
+        actual = select_race()
+        self.assertIs(type(actual), str)
+
+    @patch('builtins.input', return_value=9)
+    def test_select_race_is_string_max(self, mock_input):
+        actual = select_race()
+        self.assertIs(type(actual), str)
