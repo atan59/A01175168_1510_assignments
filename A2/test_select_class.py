ee88d735398a28bf4c96456223d647fea7f990a2
atan59 <atan59@my.bcit.ca>
2019-10-18 11:46:12 -0700

    Finished select_class function unittests.

--- A2/test_select_class.py
+++ A2/test_select_class.py
@@ -1,6 +1,27 @@
 from unittest import TestCase
+from unittest.mock import patch
+from A2.dungeonsanddragons import select_class
 
 
-class TestSelect_class(TestCase):
-    def test_select_class(self):
-        self.fail()
+class TestSelectClass(TestCase):
+    @patch('builtins.input', return_value=1)
+    def test_select_class_min(self, mock_input):
+        expected = 'barbarian'
+        actual = select_class()
+        self.assertEqual(expected, actual)
+
+    @patch('builtins.input', return_value=12)
+    def test_select_class_max(self, mock_input):
+        expected = 'wizard'
+        actual = select_class()
+        self.assertEqual(expected, actual)
+
+    @patch('builtins.input', return_value=1)
+    def test_select_class_is_string_min(self, mock_input):
+        actual = select_class()
+        self.assertEqual(type(actual), str)
+
+    @patch('builtins.input', return_value=12)
+    def test_select_class_is_string_max(self, mock_input):
+        actual = select_class()
+        self.assertEqual(type(actual), str)
