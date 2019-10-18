4bafb9991a5731e90d350c2b5b8058724983f239
atan59 <atan59@my.bcit.ca>
2019-10-18 11:20:15 -0700

    Created unittest files for all new or modified functions.

---
+++ A2/test_check_dex_roll.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestCheck_dex_roll(TestCase):
+    def test_check_dex_roll(self):
+        self.fail()
---
+++ A2/test_get_class_hit_die.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestGet_class_hit_die(TestCase):
+    def test_get_class_hit_die(self):
+        self.fail()
