8c730f8b83d495932dd93f20fb14ff6074e8542c
atan59 <atan59@my.bcit.ca>
2019-10-19 15:59:11 -0700

    Unfinished unittests

--- A2/test_create_character.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-from unittest.mock import patch
-from A2.dungeonsanddragons import create_character
-
-
-class TestCreate_character(TestCase):
-    def test_create_character(self):
-        self.fail()
