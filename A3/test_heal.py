6142f8ceb7db63e5cc47c6758d8c383252507a92
atan59 <atan59@my.bcit.ca>
2019-11-11 19:27:20 -0800

    Finished unit tests for heal

--- A3/test_heal.py
+++ A3/test_heal.py
@@ -1,6 +1,46 @@
 from unittest import TestCase
+from A3.sud import heal
 
 
 class TestHeal(TestCase):
-    def test_heal(self):
-        self.fail()
+    def test_heal_plus_two(self):
+        character = {'Name': 'Test', 'HP': [1, 10]}
+        expected = 3
+        heal(character)
+        actual = character['HP'][0]
+        self.assertEqual(expected, actual)
+
+    def test_heal_plus_two_to_full(self):
+        character = {'Name': 'Test', 'HP': [9, 10]}
+        expected = 10
+        heal(character)
+        actual = character['HP'][0]
+        self.assertEqual(expected, actual)
+
+    def test_heal_full(self):
+        character = {'Name': 'Test', 'HP': [10, 10]}
+        expected = 10
+        heal(character)
+        actual = character['HP'][0]
+        self.assertEqual(expected, actual)
+
+    def test_heal_plus_two_type(self):
+        character = {'Name': 'Test', 'HP': [1, 10]}
+        expected = type(3)
+        heal(character)
+        actual = type(character['HP'][0])
+        self.assertEqual(expected, actual)
+
+    def test_heal_plus_two_to_full_type(self):
+        character = {'Name': 'Test', 'HP': [9, 10]}
+        expected = type(10)
+        heal(character)
+        actual = type(character['HP'][0])
+        self.assertEqual(expected, actual)
+
+    def test_heal_full_type(self):
+        character = {'Name': 'Test', 'HP': [10, 10]}
+        expected = type(10)
+        heal(character)
+        actual = type(character['HP'][0])
+        self.assertEqual(expected, actual)
