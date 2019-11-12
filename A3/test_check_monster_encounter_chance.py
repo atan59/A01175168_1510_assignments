abfbb72bb82845aa31e03a4e9a9d4493913a14a0
atan59 <atan59@my.bcit.ca>
2019-11-11 19:48:05 -0800

    Finished unit tests for check_monster_encounter_chance

--- A3/test_check_monster_encounter_chance.py
+++ A3/test_check_monster_encounter_chance.py
@@ -1,6 +1,29 @@
 from unittest import TestCase
+from unittest.mock import patch
+from A3.monsters import check_monster_encounter_chance
 
 
-class TestCheck_monster_encounter_chance(TestCase):
-    def test_check_monster_encounter_chance(self):
-        self.fail()
+class TestCheckMonsterEncounterChance(TestCase):
+    @patch('random.randint', return_value=0)
+    def test_check_monster_encounter_chance_true(self, mock_randint):
+        expected = True
+        actual = check_monster_encounter_chance()
+        self.assertEqual(expected, actual)
+
+    @patch('random.randint', return_value=1)
+    def test_check_monster_encounter_chance_false(self, mock_randint):
+        expected = False
+        actual = check_monster_encounter_chance()
+        self.assertEqual(expected, actual)
+
+    @patch('random.randint', return_value=0)
+    def test_check_monster_encounter_chance_true_type(self, mock_randint):
+        expected = type(True)
+        actual = type(check_monster_encounter_chance())
+        self.assertEqual(expected, actual)
+
+    @patch('random.randint', return_value=1)
+    def test_check_monster_encounter_chance_false_type(self, mock_randint):
+        expected = type(False)
+        actual = type(check_monster_encounter_chance())
+        self.assertEqual(expected, actual)
