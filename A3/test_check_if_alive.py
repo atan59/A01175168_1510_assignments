8f10eea32b59b940d12280a9df64ba2f76efbaea
atan59 <atan59@my.bcit.ca>
2019-11-11 19:29:19 -0800

    Finished unit tests for check_if_alive

--- A3/test_check_if_alive.py
+++ A3/test_check_if_alive.py
@@ -1,6 +1,24 @@
 from unittest import TestCase
+from A3.sud import check_if_alive
 
 
 class TestCheck_if_alive(TestCase):
-    def test_check_if_alive(self):
-        self.fail()
+    def test_check_if_alive_true(self):
+        expected = True
+        actual = check_if_alive({'Name': 'Test', 'HP': [1, 10]})
+        self.assertEqual(expected, actual)
+
+    def test_check_if_alive_false(self):
+        expected = False
+        actual = check_if_alive({'Name': 'Test', 'HP': [0, 10]})
+        self.assertEqual(expected, actual)
+
+    def test_check_if_alive_true_type(self):
+        expected = type(True)
+        actual = type(check_if_alive({'Name': 'Test', 'HP': [1, 10]}))
+        self.assertEqual(expected, actual)
+
+    def test_check_if_alive_false_type(self):
+        expected = type(False)
+        actual = type(check_if_alive({'Name': 'Test', 'HP': [0, 10]}))
+        self.assertEqual(expected, actual)
