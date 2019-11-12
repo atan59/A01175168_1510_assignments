32bbd76e420ff6cd544aaadc41c4018063ba6264
atan59 <atan59@my.bcit.ca>
2019-11-11 19:50:09 -0800

    Finished unit tests for check_flee_chance

--- A3/test_check_flee_chance.py
+++ A3/test_check_flee_chance.py
@@ -1,6 +1,29 @@
 from unittest import TestCase
+from unittest.mock import patch
+from A3.monsters import check_flee_chance
 
 
-class TestCheck_flee_chance(TestCase):
-    def test_check_flee_chance(self):
-        self.fail()
+class TestCheckFleeChance(TestCase):
+    @patch('random.randint', return_value=1)
+    def test_check_flee_chance_true(self, mock_randint):
+        expected = True
+        actual = check_flee_chance()
+        self.assertEqual(expected, actual)
+
+    @patch('random.randint', return_value=0)
+    def test_check_flee_chance_false(self, mock_randint):
+        expected = False
+        actual = check_flee_chance()
+        self.assertEqual(expected, actual)
+
+    @patch('random.randint', return_value=1)
+    def test_check_flee_chance_true_type(self, mock_randint):
+        expected = type(True)
+        actual = type(check_flee_chance())
+        self.assertEqual(expected, actual)
+
+    @patch('random.randint', return_value=0)
+    def test_check_flee_chance_false_type(self, mock_randint):
+        expected = type(False)
+        actual = type(check_flee_chance())
+        self.assertEqual(expected, actual)
