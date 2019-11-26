fb52ed4d4ed5fb8ead6b1c21c8109ab9e0d1c473
atan59 <atan59@my.bcit.ca>
2019-11-26 15:02:01 -0800

    Finished docstrings with doctests for Question 4.

--- A4/question_4.py
+++ A4/question_4.py
@@ -1,7 +1,18 @@
 """Sort items in a list using selection sort algorithm."""
+import doctest
 
 
 def swap_elements(item_list: list, i: int) -> list:
+    """
+    Swap smaller elements with larger elements.
+
+    :param item_list: a list
+    :param i: an int
+    :precondition: item_list must be a non-empty sortable list
+    :precondition: i must be a positive integer
+    :postcondition: Swap the lower element to the beginning
+    :return: a list
+    """
     for j in range(i + 1, len(item_list)):
         if item_list[i] > item_list[j]:
             item_list[i], item_list[j] = item_list[j], item_list[i]
@@ -9,6 +20,14 @@ def swap_elements(item_list: list, i: int) -> list:
 
 
 def selection_sort(item_list: list) -> list:
+    """
+    Selection sort a list of items.
+
+    :param item_list: a list
+    :precondition: item_list must be a non-empty sortable list
+    :postcondition: Sort item_list using selection sort algorithm
+    :return: a list
+    """
     try:
         if not item_list:
             raise ValueError("Empty list is not a valid value.")
@@ -18,3 +37,7 @@ def selection_sort(item_list: list) -> list:
         print("Error: " + str(e))
     else:
         return item_list
+
+
+if __name__ == "__main__":
+    doctest.testmod()
