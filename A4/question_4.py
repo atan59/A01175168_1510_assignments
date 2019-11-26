8527e150f5552ef952da060ff1d5e0bf79ca4af9
atan59 <atan59@my.bcit.ca>
2019-11-26 15:11:12 -0800

    Finished docstrings with doctests for Question 4.

--- A4/question_4.py
+++ A4/question_4.py
@@ -12,6 +12,13 @@ def swap_elements(item_list: list, i: int) -> list:
     :precondition: i must be a positive integer
     :postcondition: Swap the lower element to the beginning
     :return: a list
+
+    >>> swap_elements(['c', 'a', 'b'], 0)
+    ['a', 'c', 'b']
+    >>> swap_elements([4, 2, 7, 1], 0)
+    [1, 4, 7, 2]
+    >>> swap_elements([0.5, 1, 0, 4], 0)
+    [0, 1, 0.5, 4]
     """
     for j in range(i + 1, len(item_list)):
         if item_list[i] > item_list[j]:
@@ -27,6 +34,13 @@ def selection_sort(item_list: list) -> list:
     :precondition: item_list must be a non-empty sortable list
     :postcondition: Sort item_list using selection sort algorithm
     :return: a list
+
+    >>> selection_sort([3, 1, 5, 7, 2])
+    [1, 2, 3, 5, 7]
+    >>> selection_sort(['b', 'a', 'd', 'z', 'c'])
+    ['a', 'b', 'c', 'd', 'z']
+    >>> selection_sort([0.5, 0, 5, 2])
+    [0, 0.5, 2, 5]
     """
     try:
         if not item_list:
