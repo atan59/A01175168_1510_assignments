cec970dac8b579cb8d2080a3b8f137ec1996d423
atan59 <atan59@my.bcit.ca>
2019-11-26 20:46:10 -0800

    Finished unit test for swap_elements. Finished question 6.

---
+++ A4/quesiton_6.py
@@ -0,0 +1,11 @@
+"""Answer to question 6."""
+
+"""
+I would first write a unit test to see if two lines that don't intersect will return None.
+The second test would be to test if the correct coordinates are return when two lines intersect at one point.
+The third test would be to test if the function returns the first argument when two lines lay on top of each other.
+For the forth test, I would test if the function will throw an error if the arguments are not lines.
+For the fifth test, I would test if a line with floats as points and doesn't intersect will return None.
+For the sixth test, I would test if a line with floats as points that intersect will return the point where they
+intersect.
+"""
--- A4/test_swap_elements.py
+++ A4/test_swap_elements.py
@@ -1,6 +1,34 @@
 from unittest import TestCase
+from A4.question_4 import swap_elements
 
 
 class TestSwapElements(TestCase):
-    def test_swap_elements(self):
-        self.fail()
+    def test_swap_elements_already_in_order_strings(self):
+        actual = swap_elements(['a', 'b', 'e', 'c'], 0)
+        expected = ['a', 'b', 'e', 'c']
+        self.assertEqual(expected, actual)
+
+    def test_swap_elements_lowest_at_end_strings(self):
+        actual = swap_elements(['z', 'b', 'e', 'a'], 0)
+        expected = ['a', 'z', 'e', 'b']
+        self.assertEqual(expected, actual)
+
+    def test_swap_elements_lowest_next_to_index_strings(self):
+        actual = swap_elements(['b', 'a', 'e', 'c'], 0)
+        expected = ['a', 'b', 'e', 'c']
+        self.assertEqual(expected, actual)
+
+    def test_swap_elements_already_in_order_numbers(self):
+        actual = swap_elements([1, 2, 4, 3], 0)
+        expected = [1, 2, 4, 3]
+        self.assertEqual(expected, actual)
+
+    def test_swap_elements_lowest_at_end_numbers(self):
+        actual = swap_elements([3, 2, 4, 1], 0)
+        expected = [1, 3, 4, 2]
+        self.assertEqual(expected, actual)
+
+    def test_swap_elements_lowest_next_to_index_numbers(self):
+        actual = swap_elements([2, 1, 4, 3], 0)
+        expected = [1, 2, 4, 3]
+        self.assertEqual(expected, actual)
