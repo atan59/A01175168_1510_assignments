70c1dd95e135f50dfd8a0815f4c822f1ba5b34ed
atan59 <atan59@my.bcit.ca>
2019-11-26 22:47:45 -0800

    Finished unit test for check_if_higher.

--- A4/test_check_if_higher.py
+++ A4/test_check_if_higher.py
@@ -1,6 +1,29 @@
 from unittest import TestCase
+from A4.question_8 import check_if_higher
 
 
-class TestCheck_if_higher(TestCase):
-    def test_check_if_higher(self):
-        self.fail()
+class TestCheckIfHigher(TestCase):
+    def test_check_if_higher_true(self):
+        actual = check_if_higher({"0:00": 18}, 21)
+        expected = ["0:00"]
+        self.assertEqual(expected, actual)
+
+    def test_check_if_higher_false(self):
+        actual = check_if_higher({"0:00": 18}, 6)
+        expected = []
+        self.assertEqual(expected, actual)
+
+    def test_check_if_higher_true_multiple(self):
+        actual = check_if_higher({"0:00": 18, "1:11": 6}, 21)
+        expected = ["0:00", "1:11"]
+        self.assertEqual(expected, actual)
+
+    def test_check_if_higher_false_multiple(self):
+        actual = check_if_higher({"0:00": 18, "1:12": 9}, 6)
+        expected = []
+        self.assertEqual(expected, actual)
+
+    def test_check_if_higher_some_true_some_false(self):
+        actual = check_if_higher({"0:00": 18, "1:12": 9}, 14)
+        expected = ["1:12"]
+        self.assertEqual(expected, actual)
