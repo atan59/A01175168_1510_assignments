832c708b1482378f5bb6cbd827b6b12384d4add1
atan59 <atan59@my.bcit.ca>
2019-11-26 22:50:47 -0800

    Finished unit test for im_not_sleepy.

--- A4/test_im_not_sleepy.py
+++ A4/test_im_not_sleepy.py
@@ -1,6 +1,17 @@
 from unittest import TestCase
+from unittest.mock import patch
+from A4.question_8 import im_not_sleepy
+import io
 
 
 class TestIm_not_sleepy(TestCase):
-    def test_im_not_sleepy(self):
-        self.fail()
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_im_not_sleepy_output(self, mock_stdout):
+        im_not_sleepy()
+        actual = mock_stdout.getvalue()
+        expected = "10:08 requires the most bars. It requires: 21 bars to display.\n"
+
+    def test_im_not_sleepy_type(self):
+        actual = type(im_not_sleepy())
+        expected = type("10:08 requires the most bars. It requires: 21 bars to display.\n")
+        self.assertEqual(expected, actual)
