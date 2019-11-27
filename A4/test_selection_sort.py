916fab0fc991c4ebc2bd136c56652e254cf0baae
atan59 <atan59@my.bcit.ca>
2019-11-26 16:43:55 -0800

    Created unit tests for Question 4.

---
+++ A4/test_selection_sort.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestSelectionSort(TestCase):
+    def test_selection_sort(self):
+        self.fail()
---
+++ A4/test_swap_elements.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestSwapElements(TestCase):
+    def test_swap_elements(self):
+        self.fail()
